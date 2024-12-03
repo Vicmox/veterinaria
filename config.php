75f6e9f8dd021e7d227184c8f50ba9ef3174e617
Victor Monsalvo <victormanuelms@ufps.edu.co>
2024-12-02 23:50:31 -0500

    "change(bd): Enviroment keys

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('APP_NAME', 'Sistema de Información Clínica Veterinaria PETS HOME');
 define('SERVIDOR', 'mysql.railway.internal'); // Cambia por el hostname proporcionado por Railway
 define('USUARIO', 'root');
-define('PASSWORD', 'tu_contraseña'); // Reemplaza con la contraseña correcta
+define('PASSWORD', 'DB_PASS'); // Reemplaza con la contraseña correcta
 define('BD', 'clinicaveterinaria');
 
 $conexion = mysqli_connect(SERVIDOR, USUARIO, PASSWORD, BD);
