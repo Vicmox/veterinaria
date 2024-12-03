7fe92b5a9e578381deb981af5364fbc3187243eb
Victor Monsalvo <victormanuelms@ufps.edu.co>
2024-12-02 23:48:53 -0500

    change(bd): cambio en config.php

--- config.php
+++ config.php
@@ -1,20 +1,14 @@
 <?php
 define('APP_NAME', 'Sistema de Información Clínica Veterinaria PETS HOME');
-define('SERVIDOR', '127.0.0.1');  // O 'localhost'
-define('USUARIO', 'root');         // Usuario por defecto en MySQL
-define('PASSWORD', '');            // Contraseña si tiene, o vacío si no
-define('BD', 'clinicaveterinaria'); // Nombre de la base de datos
+define('SERVIDOR', 'mysql.railway.internal'); // Cambia por el hostname proporcionado por Railway
+define('USUARIO', 'root');
+define('PASSWORD', 'tu_contraseña'); // Reemplaza con la contraseña correcta
+define('BD', 'clinicaveterinaria');
 
-$servidor = "mysql:host=" . SERVIDOR . ";dbname=" . BD;
+$conexion = mysqli_connect(SERVIDOR, USUARIO, PASSWORD, BD);
 
-date_default_timezone_set("America/Bogota");
-$fechaHora = date("Y-m-d H:i:s");
-
-try {
-    $pdo = new PDO($servidor, USUARIO, PASSWORD);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<script>console.log('Connected to the database successfully!');</script>";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+if (!$conexion) {
+    die("Conexión fallida: " . mysqli_connect_error());
 }
+echo "Conectado correctamente a la base de datos.";
 ?>
