7dae0a99e2c89bf6430f953b948e81eed8906270
Victor Monsalvo <victormanuelms@ufps.edu.co>
2024-12-02 23:28:09 -0500

    change(bd): Connection mysql database

--- config.php
+++ config.php
@@ -1,26 +1,20 @@
 <?php
 define('APP_NAME', 'Sistema de Información Clínica Veterinaria PETS HOME');
+define('SERVIDOR', '127.0.0.1');  // O 'localhost'
+define('USUARIO', 'root');         // Usuario por defecto en MySQL
+define('PASSWORD', '');            // Contraseña si tiene, o vacío si no
+define('BD', 'clinicaveterinaria'); // Nombre de la base de datos
 
-// Obtén las credenciales desde las variables de entorno
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_PORT', getenv('DB_PORT'));
+$servidor = "mysql:host=" . SERVIDOR . ";dbname=" . BD;
 
-
-// Construcción de la cadena de conexión PDO
-$servidor = "mysql:host=" . DB_HOST . ";port=" . getenv('DB_PORT') . ";dbname=" . DB_NAME;
-
-// Zona horaria
 date_default_timezone_set("America/Bogota");
 $fechaHora = date("Y-m-d H:i:s");
 
-// Conexión a la base de datos
 try {
-    $pdo = new PDO($servidor, DB_USER, DB_PASS);
+    $pdo = new PDO($servidor, USUARIO, PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "<script>console.log('Connected to the database successfully!');</script>";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+?>
